a721dbb347ef91c36153cad894dd2f967dad9c16
git-uday-kiran <udaykiran0486@gmail.com>
2025-09-13 12:27:30 +0530

    basic hello task scheduler

--- src/main/java/bit/bees/taskscheduler/TaskSchedulerService.java
+++ src/main/java/bit/bees/taskscheduler/TaskSchedulerService.java
@@ -3,14 +3,17 @@ package bit.bees.taskscheduler;
 import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.boot.context.event.ApplicationStartedEvent;
-import org.springframework.context.event.EventListener;
+import org.springframework.beans.BeansException;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.lang.NonNull;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Task Scheduler, schedules tasks every 10 seconds
@@ -20,34 +23,29 @@ import java.time.format.DateTimeFormatter;
  */
 
 @Service
-public class TaskSchedulerService {
+public class TaskSchedulerService implements ApplicationContextAware {
 
-    private long executionCount = 0;
-    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss a");
     private long startTime;
+    private long executionCount = 0;
+    private ApplicationContext applicationContext;
 
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss a");
     private final Logger log = LoggerFactory.getLogger(TaskSchedulerService.class);
 
     @PostConstruct
     public void postConstruct() {
         resolveStartTime();
-        log.info("✅ Task Scheduler initialized at {}", getCurrentTimeStamp());
+        log.info("✅ Task Scheduler initialized at {}", getCurrentTimestamp());
         log.info("🗓 Will run for 60 seconds, executing every 10 seconds");
         log.info("⏰ Expected executions: 6\n");
     }
 
     @Scheduled(fixedRate = 10_000)
     public void executeHelloTask() {
-        resolveStartTime();
-        long elapsedTime = (System.currentTimeMillis() - startTime) / 1000;
-        if (elapsedTime >= 60) {
-            log.info("🔕 Elapsed 60 seconds, shutting down");
-            System.exit(0);
-        }
-
         incrementExecutionCount();
-        var timeStamp = getCurrentTimeStamp();
-        String message = "Hello from Task Scheduler | Execution #%d | Time: %s | Elapsed time: %s seconds".formatted(getExecutionCount(), timeStamp, elapsedTime);
+        var elapsedTime = getElapsedTime();
+        var timestamp = getCurrentTimestamp();
+        String message = "Hello from Task Scheduler | Execution #%d | Timestamp: %s | Elapsed time: %ss".formatted(getExecutionCount(), timestamp, elapsedTime);
         log.info(message);
 
         if (getExecutionCount() == 1) {
@@ -57,7 +55,21 @@ public class TaskSchedulerService {
         }
     }
 
-    public String getCurrentTimeStamp() {
+    @Scheduled(fixedRate = 100, timeUnit = TimeUnit.MILLISECONDS)
+    private void monitorElapsedTime() {
+        resolveStartTime();
+        long elapsedTime = getElapsedTime();
+        if (elapsedTime >= 60) {
+            log.info("🔕 Elapsed 60 seconds, shutting down gracefully...");
+            SpringApplication.exit(applicationContext);
+        }
+    }
+
+    private long getElapsedTime() {
+        return (System.currentTimeMillis() - startTime) / 1000;
+    }
+
+    public String getCurrentTimestamp() {
         return LocalDateTime.now().format(formatter);
     }
 
@@ -73,5 +85,10 @@ public class TaskSchedulerService {
 
     private synchronized void incrementExecutionCount() {
         executionCount++;
+    }
+
+    @Override
+    public void setApplicationContext(@NonNull ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
     }
 }
